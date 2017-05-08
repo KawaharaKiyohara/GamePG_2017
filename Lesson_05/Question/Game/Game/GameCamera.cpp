d89562a85ae731495f7f220ae2b3b3864c3366cf
TakayukiKiyohara <nezumimusume30@gmail.com>
2017-05-09 07:19:25 +0900

    Lesson_05を追加

---
+++ Lesson_05/Question/Game/Game/GameCamera.cpp
@@ -0,0 +1,43 @@
+/*!
+ *@brief	ゲームカメラ。
+ */
+#include "stdafx.h"
+#include "GameCamera.h"
+#include "Player.h"
+
+GameCamera* gameCamera = nullptr;
+
+
+GameCamera::GameCamera()
+{
+}
+GameCamera::~GameCamera()
+{
+}
+bool GameCamera::Start()
+{
+	//カメラの初期化。
+	camera.SetNear(0.01f);
+	camera.SetFar(10000.0f);
+	camera.SetPosition(CVector3(0.0f, 1.5f, -4.0f));
+	camera.SetTarget(CVector3(0.0f, 1.0f, 0.0f));
+	camera.SetUp(CVector3::Up);
+	camera.SetViewAngle(CMath::DegToRad(60.0f));
+	
+	return true;
+}
+void GameCamera::Update()
+{
+	Move();
+	camera.Update();
+}
+
+//////////////////////////////////////////////////////////////////////
+// カメラを動かすプログラムが記述されてるよ。
+//////////////////////////////////////////////////////////////////////
+void GameCamera::Move()
+{
+	CVector3 playerPos = player->GetPosition();
+	//Question 1 注視点を動かす。
+	//Question 2 視点を動かす。
+}
